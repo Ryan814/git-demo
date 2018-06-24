8740d5d3c6ae0f3220283350dda2eac3caba60d3
Ryan <ryan@gmail.com>
2018-06-24 14:26:05 +0800

    merge index & show branches

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,10 @@
 class UsersController < ApplicationController
-<<<<<<< HEAD
 	def index
 	   @users = User.all
   end
-=======
 
-def show
-  @user = User.find(params[:id])
-end
+	def show
+	  @user = User.find(params[:id])
+	end
 
->>>>>>> e4e500092dc67b38da2a9cf6c194acfa78927dd2
 end
